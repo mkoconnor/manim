c7f44372cb0d480dc278e378e7a37c6cd3d8977f
Michael O'Connor <mkoconnor@gmail.com>
2017-05-24 21:47:40 -0400

    display some matchings

--- eost/scene1.py
+++ eost/scene1.py
@@ -6,6 +6,7 @@ import numpy as np
 import helpers
 from helpers import *
 from animation.transform import *
+from topics.geometry import *
 
 def in_a_row(mobj,n):
     mobjs = []
@@ -17,6 +18,15 @@ def in_a_row(mobj,n):
         last_mobj = this_mobj
         mobjs.append(this_mobj)
     return mobjs
+
+def line(from_,to):
+    return Line(from_.get_center(),to.get_center())
+
+def permute(l):
+    import random
+    ret = l[:]
+    random.shuffle(ret)
+    return ret
 
 def permute_animations(mobjs,move):
     permuted_indices = np.random.permutation(len(mobjs))
@@ -34,16 +44,33 @@ def permute_animations(mobjs,move):
 
 class Scene1(Scene):
     def construct(self):
-        apples = in_a_row(Apple(),5)
-        pears = in_a_row(Pear(),4)
+        num_apples = 5
+        num_pears = 4
+        min_fruit = min(num_apples,num_pears)
+        apples = in_a_row(Apple(),num_apples)
+        pears = in_a_row(Pear(),num_pears)
         apple_group = Group(*apples).center().to_edge(UP)
         pear_group = Group(*pears).center().to_edge(DOWN)
         # Display the two lines of apples and pears
         self.play(ShowCreation(apple_group))
         self.play(ShowCreation(pear_group))
         self.dither()
+        # Show a matching
+        matching = Group(*map(line,apples[:min_fruit],pears[:min_fruit]))
+        self.play(ShowCreation(matching))
+        self.dither()
+        self.play(Uncreate(matching))
         # Permute them
         apple_permutations=permute_animations(apple_group,move="down")
         pear_permutations=permute_animations(pear_group,move="up")
         self.play(*(apple_permutations + pear_permutations))
+        # Show a different matching
+        matching = Group(*map(
+            line,
+            permute(apples)[:min_fruit],
+            permute(pears)[:min_fruit]
+        ))
+        self.play(ShowCreation(matching))
+        self.dither()
+        self.play(Uncreate(matching))
         self.dither()
