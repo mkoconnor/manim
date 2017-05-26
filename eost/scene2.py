71b0e383a2649628326012cb937c40ef4ce4cb9b
Michael O'Connor <mkoconnor@gmail.com>
2017-05-25 22:40:04 -0400

    remove dots, change labels

--- eost/scene2.py
+++ eost/scene2.py
@@ -8,42 +8,29 @@ from animation.simple_animations import *
 from animation.transform import *
 
 class NumberLine(Mobject):
-    def __init__(self,mobj,n=7):
+    def __init__(self,mobj,direction,n=7):
         unnumbered_mobjs = []
         numbered_mobjs = []
 
         def make_number_mobj(n):
             text = TextMobject(str(n))
-            dot = Dot().next_to(text,direction=DOWN)
-            Group(text,dot).center()
             buff = Rectangle(color=BLACK,height=1, width=1.9)
-            return Group(buff,text,dot)
+            return Group(buff,text)
 
         for i in xrange(n):
             copy = mobj.copy()
             unnumbered_mobjs.append(copy)
             number_mobj = make_number_mobj(i)
-            number_mobj.next_to(copy,direction=DOWN)
+            number_mobj.next_to(copy,direction=direction)
             numbered_mobjs.append(Group(copy,number_mobj))
 
-        last_numbered_mobj = None
-        for numbered_mobj in numbered_mobjs:
-            if last_numbered_mobj is not None:
-                numbered_mobj.next_to(last_numbered_mobj)
-            last_numbered_mobj = numbered_mobj
-        self.unnumbered_mobjs = unnumbered_mobjs
-        self.numbered_mobjs = numbered_mobjs
         Mobject.__init__(self, *numbered_mobjs)
+        self.arrange_submobjects()
 
 class Scene2(Scene):
     def construct(self):
-        # I think the apples and pears look better a little smaller
-        # than they did in scene 1
-        scale_factor = 0.9
-        apple = Apple().scale_in_place(scale_factor)
-        pear = Pear().scale_in_place(scale_factor)
-        apples = NumberLine(apple).to_edge(UP).to_edge(LEFT)
-        pears = NumberLine(pear).to_edge(DOWN).to_edge(LEFT)
+        apples = NumberLine(Apple(),direction=UP).to_edge(UP).to_edge(LEFT)
+        pears = NumberLine(Pear(),direction=DOWN).to_edge(DOWN).to_edge(LEFT)
         self.play(ShowCreation(apples))
         self.play(ShowCreation(pears))
         self.dither()
