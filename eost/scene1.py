550389bf4a6a8f67cdddd03e017d4dc7ff799a14
Michael O'Connor <mkoconnor@gmail.com>
2017-05-27 09:42:08 -0400

    make determinstic

--- eost/scene1.py
+++ eost/scene1.py
@@ -10,8 +10,11 @@ from animation.transform import *
 from topics.geometry import *
 from .matching import get_matching, MatchingAnimations
 
+import random
+random.seed(489903318756316287)
+np.random.seed(2442432778)
+
 def permute(l):
-    import random
     ret = l[:]
     random.shuffle(ret)
     return ret
